c713c4c10ca1f20112237a894123002a23ec6e85
Rusman <ifrusman@gmail.com>
2020-08-07 11:51:38 +0700

    Testing TFSEC Failed

--- main.tf
+++ main.tf
@@ -3,23 +3,9 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_iam_policy" "basically_allow_all" {
-  name        = "some_policy"
-  path        = "/"
-  description = "Some policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "*",
-      "Effect": "Allow",
-      "Resource": "Some policy with a long description that denies anything"
-    }
-  ]
-}
-EOF
+resource "aws_security_group_rule" "my-rule" {
+    type        = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_iam_policy" "basically_deny_all" {
